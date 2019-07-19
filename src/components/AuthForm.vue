e4fae3b4c16f9fef0ac80c2c1cc6f5fe4a0e0c75
尹锐杰 <yingong@yinruijies-MacBook-Pro.local>
2019-07-19 22:04:00 +0800

    before adding bootstrap

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form action>
+    <input type="text" value="username" />
+    <input type="text" value="password" />
+    <template v-if="!mode">
+      <input type="text" value="username" />
+      <input type="text" value="password" />
+    </template>
+    <!-- TODO:add boostrap  -->
+  </form>
+</template>
+
+<script>
+import { log } from "util";
+export default {
+  props: ["mode"],
+  data: function(props) {
+    return {};
+  }
+};
+</script>
---
+++ src/views/Auth.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is {{Number(mode)?'Signin':'Signup'}} page</h1>
+    <Form :mode="Number(mode)"></Form>
+  </div>
+</template>
+
+<script>
+import Form from "../components/AuthForm.vue";
+export default {
+  components: {
+    Form
+  },
+  data: function() {
+    return {
+      mode: this.$route.params.mode
+    };
+  },
+  watch: {
+    $route(to, from) {
+      this.mode = to.params["mode"];
+    }
+  }
+};
+</script>
